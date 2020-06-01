e52631496ac50501f5b0d55403c21c318ba0b1f5
Michael SM <michelsetyawanm@gmail.com>
2020-06-01 12:20:20 +0700

    Ini adalah state

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Mike');
+  const [person, setPerson] = useState({name: 'Joana', age: 20})
+  const[animal, setAnimal] = useState({name: 'Chocky', type: 'Husky'});
+
+  const clickHandler = () => {
+    setName('Michel Setyawan Mulyono');
+    setPerson({name: 'Bonita', age: 20});
+    setAnimal({name: 'Bruno', type: 'Golden'});
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My Name is {name}</Text>
+      <Text>Her name is {person.name} and Her age is {person.age}</Text>
+      <Text>They have animal called {animal.name} and the type is {animal.type}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler} />
+      </View>
     </View>
   );
 }
@@ -16,4 +31,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 20,
+  }
 });
